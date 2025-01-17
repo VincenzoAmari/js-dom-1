c5f08ccdddf6c7be1dcadd2af1140f26439a31ec
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-17 17:40:11 +0100

    si accende ma non si spegne più ... è qualcosa

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("accendiButton").addEventListener("click", function () {
+  const lampadina = document.getElementById("lampadina");
+  lampadina.src = "img/Yellow_lamp.png";
+});
