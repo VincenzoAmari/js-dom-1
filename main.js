b165b5aa02ffa17f802f89ef450233deb0258095
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-17 18:02:59 +0100

    ci siamo, aggiusto 2 cosine

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-document.getElementById("accendiButton").addEventListener("click", function () {
-  const lampadina = document.getElementById("lampadina");
-  lampadina.src = "img/Yellow_lamp.png";
-});
+document
+  .getElementById("accendiSpegniButton")
+  .addEventListener("click", function () {
+    const lampadina = document.getElementById("lampadina");
+    const button = document.getElementById("accendiSpegniButton");
+
+    if (lampadina.src.includes("img/white_lamp.png")) {
+      lampadina.src = "img/Yellow_lamp.png";
+      button.textContent = "Spegni";
+    } else {
+      lampadina.src = "img/white_lamp.png";
+      button.textContent = "Accendi";
+    }
+  });
