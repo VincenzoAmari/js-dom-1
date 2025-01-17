3b11af6f76b8a0a09e1b4fbc7057065c1ee42bf2
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-17 18:24:47 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-document
-  .getElementById("accendiSpegniButton")
-  .addEventListener("click", function () {
-    const lampadina = document.getElementById("lampadina");
-    const button = document.getElementById("accendiSpegniButton");
+const lampadina = document.getElementById("lampadina");
+const button = document.getElementById("accendiSpegniButton");
 
-    if (lampadina.src.includes("img/white_lamp.png")) {
-      lampadina.src = "img/Yellow_lamp.png";
-      button.textContent = "Spegni";
-    } else {
-      lampadina.src = "img/white_lamp.png";
-      button.textContent = "Accendi";
-    }
-  });
+button.addEventListener("click", function () {
+  if (lampadina.src.includes("white_lamp.png")) {
+    lampadina.src = "img/Yellow_lamp.png";
+    button.textContent = "Spegni";
+  } else {
+    lampadina.src = "img/white_lamp.png";
+    button.textContent = "Accendi";
+  }
+});
